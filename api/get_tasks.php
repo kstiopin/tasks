ba471c86faf3f587e578cfa59964f5216d901689
kstiopin <kirill.stiopin@hotmail.com>
2018-07-05 13:37:55 +0300

    added styles/details/priority marks to tasks list; added ordering in SQL request

--- api/get_tasks.php
+++ api/get_tasks.php
@@ -2,8 +2,9 @@
 require_once '../config/config_public.php';
 
 $result = [];
-$r = $mysqli->query("SELECT * FROM `tasks`");
+$r = $mysqli->query("SELECT * FROM tasks ORDER BY state, urgency, added");
 while ($f = $r->fetch_assoc()) {
+    $f['details'] = nl2br($f['details']);
     $result[] = $f;
 }
 
--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 
 import { getTasksAction } from './actions';
 
+const urgencyMarks = ['critical.svg', 'major.svg', 'major.svg', 'neutral.png', 'trivial.svg', 'minor.svg', 'minor.svg'];
+
 class App extends Component {
+  state = { hideDone: true }
+
   componentDidMount() {
     const { getTasks } = this.props;
     getTasks();
   }
 
+  toggleShowDone = () => this.setState(prevState => ({ hideDone: !prevState.hideDone }))
+
   render () {
     const { tasks } = this.props;
+    const { hideDone } = this.state;
 
-    return tasks.map(task => <div key={ task.id }>
-      { task.text }
-    </div>);
+    return (<Fragment>
+      <div className='controlls'>
+        <span onClick={ this.toggleShowDone }>{ hideDone ? 'Show' : 'Hide'} 'Done' tasks</span>
+      </div>
+      <div className='tasks'>
+        { tasks.map(task => (!hideDone || (task.state === 'todo')) && <div key={ task.id } className={ task.state }>
+          <img src={ `style/${urgencyMarks[task.urgency - 1]}` } alt={ task.urgency } title={ task.added } />
+          <div>{ task.text }</div>
+          <em dangerouslySetInnerHTML={ {__html: task.details } } />
+        </div>) }
+      </div>
+    </Fragment>);
   }
 }
 
