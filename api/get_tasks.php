942512bd4ed5deb0ba44f83648cf9eb431d2a0bc
kstiopin <salvation13xs@gmail.com>
2018-10-29 11:57:46 +0200

    allow tasks nesting + handle tasks sorting on the FE using JS

--- api/get_tasks.php
+++ api/get_tasks.php
@@ -2,7 +2,7 @@
 require_once '../config/config_public.php';
 
 $result = [];
-$r = $mysqli->query("SELECT * FROM tasks ORDER BY state, urgency, added");
+$r = $mysqli->query("SELECT * FROM tasks");
 while ($f = $r->fetch_assoc()) {
     $f['details'] = nl2br($f['details']);
     $result[] = $f;
--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { getTasksAction, setTaskStateAction } from './actions';
 import '../style/style.less';
 
 const urgencyMarks = ['critical.svg', 'major.svg', 'high.svg', 'neutral.png', 'small.svg', 'minor.svg', 'trivial.svg'];
+const sortTasksKeys = ['state', 'urgency', 'added'];
 
 class App extends Component {
   state = { hideDone: true }
@@ -25,24 +26,51 @@ class App extends Component {
     setTaskState(taskId, state);
   }
 
+  handleTasksSort = (task1, task2, sortTasksKey = 0) => (task1[sortTasksKeys[sortTasksKey]] > task2[sortTasksKeys[sortTasksKey]])
+    ? 1
+    : ((task1[sortTasksKeys[sortTasksKey]] < task2[sortTasksKeys[sortTasksKey]])
+      ? -1
+      : ((sortTasksKey < sortTasksKeys.length)
+        ? this.handleTasksSort(task1, task2, sortTasksKey + 1)
+        : 0))
+
+  renderTask = task => (<div key={ task.id } className={ task.state }>
+    <img src={ `style/${urgencyMarks[task.urgency - 1]}` } alt={ task.urgency } title={ task.added } />
+    <div className='task-body'>
+      { task.text }
+      { (task.state === 'todo') && <img src='../style/ok.png' alt='Finish' onClick={ () => this.setTaskState(task.id, 'done') } /> }
+      { (task.state === 'done') && <img src='../style/enable.png' alt='ReEnable' onClick={ () => this.setTaskState(task.id, 'todo') } /> }
+    </div>
+    <em dangerouslySetInnerHTML={ { __html: task.details } } />
+    { task.children && (task.children.length > 0) && task.children.map(this.renderTask) }
+  </div>)
+
   render () {
     const { tasks } = this.props;
     const { hideDone } = this.state;
+    const _tasks = {};
+    tasks.forEach(task => {
+      const taskParent = parseInt(task.parent);
+      const taskId = parseInt(task.id);
+      if (taskParent === 0) {
+        _tasks[taskId] = { ..._tasks[taskId], ...task };
+      } else {
+        if (!_tasks.hasOwnProperty(taskParent)) {
+          _tasks[taskParent] = { children: [] };
+        } else if (!_tasks[taskParent].hasOwnProperty('children')) {
+          _tasks[taskParent].children = [task];
+        } else {
+          _tasks[taskParent].children.push(task);
+        }
+      }
+    });
 
     return (<Fragment>
       <div className='controlls'>
         <span onClick={ this.toggleShowDone }>{ hideDone ? 'Show' : 'Hide'} 'Done' tasks</span>
       </div>
       <div className='tasks'>
-        { tasks.map(task => (!hideDone || (task.state === 'todo')) && <div key={ task.id } className={ task.state }>
-          <img src={ `style/${urgencyMarks[task.urgency - 1]}` } alt={ task.urgency } title={ task.added } />
-          <div>
-            { task.text }
-            { (task.state === 'todo') && <img src='style/ok.png' alt='Finish' onClick={ () => this.setTaskState(task.id, 'done') } /> }
-            { (task.state === 'done') && <img src='style/enable.png' alt='ReEnable' onClick={ () => this.setTaskState(task.id, 'todo') } /> }
-          </div>
-          <em dangerouslySetInnerHTML={ {__html: task.details } } />
-        </div>) }
+        { Object.values(_tasks).sort(this.handleTasksSort).map(task => (!hideDone || (task.state === 'todo')) && this.renderTask(task)) }
       </div>
     </Fragment>);
   }
@@ -54,7 +82,6 @@ App.propTypes = {
 };
 
 const mapStateToProps = ({ tasks }) => ({ tasks });
-
 const mapDispatchToProps = dispatch => bindActionCreators({
   getTasks: getTasksAction,
   setTaskState: setTaskStateAction
