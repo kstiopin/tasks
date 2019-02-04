0dcf71e104720d6031152600bf1a98f4249d6a1d
kstiopin <salvation13xs@gmail.com>
2019-02-04 12:38:31 +0200

    hide tasks with urgency 8

--- api/get_tasks.php
+++ api/get_tasks.php
@@ -2,7 +2,7 @@
 require_once '../config/config_public.php';
 
 $result = [];
-$r = $mysqli->query("SELECT * FROM tasks");
+$r = $mysqli->query("SELECT * FROM tasks WHERE urgency < 8");
 while ($f = $r->fetch_assoc()) {
     $f['details'] = nl2br($f['details']);
     $result[] = $f;
